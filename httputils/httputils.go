0f8a292cb4dbf094b7452ea00fc5e32927e85b4e
martin-montas <martinmontas1@gmail.com>
2025-10-26 14:43:58 -0400

    Changed some values on httputils.go, added to gitignore etc

--- httputils/httputils.go
+++ httputils/httputils.go
@@ -19,13 +19,13 @@ func NewHTTPClient() *HTTPClient {
 	client := &http.Client{
 		Timeout: 10 * time.Second, // total request timeout
 		Transport: &http.Transport{
-			MaxIdleConns:        100,              // total idle connections
+			MaxIdleConns:        200,              // total idle connections
 			MaxIdleConnsPerHost: 100,              // idle per-host (boosts reuse)
 			MaxConnsPerHost:     100,              // hard cap of connections per host
 			IdleConnTimeout:     90 * time.Second, // how long to keep idle conns alive
 			DisableKeepAlives:   false,            // keep connection open (reused)
 			DialContext: (&net.Dialer{
-				Timeout:   5 * time.Second,
+				Timeout:   10 * time.Second,
 				KeepAlive: 30 * time.Second,
 			}).DialContext,
 			TLSHandshakeTimeout:   5 * time.Second,
